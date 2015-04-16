a8067702ed11d30fca898f82d43d1b2a5309cef5
Rakshit Pithadia <rpithadia25@gmail.com>
2015-04-15 22:35:35 -0700

    Initial Commit

---
+++ SharePic/app/src/main/java/edu/sdsu/cs/sharepic/classes/Profile.java
@@ -0,0 +1,9 @@
+package edu.sdsu.cs.sharepic.classes;
+
+/**
+ * Created by rakshitpithadia on 4/15/15.
+ * Copyright (c) 2015 Harsh Shah, Rakshit Pithadia. All rights reserved.
+ */
+public class Profile {
+
+}
