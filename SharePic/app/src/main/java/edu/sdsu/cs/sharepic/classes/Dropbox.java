0188cac5e7f89d85e615932d53b89d746dec4194
Harsh <shahmharsh@gmail.com>
2015-04-20 16:02:49 -0700

    Refactoring class name, made account abstract

--- SharePic/app/src/main/java/edu/sdsu/cs/sharepic/classes/Dropbox.java
+++ SharePic/app/src/main/java/edu/sdsu/cs/sharepic/classes/Dropbox.java
@@ -4,5 +4,5 @@ package edu.sdsu.cs.sharepic.classes;
  * Created by Rakshit Pithadia on 4/15/15.
  * Copyright (c) 2015 Harsh Shah, Rakshit Pithadia. All rights reserved.
  */
-public class Dropbox extends AbstractAccount {
+public class Dropbox extends Account {
 }
--- SharePic/app/src/main/java/edu/sdsu/cs/sharepic/classes/Flickr.java
+++ SharePic/app/src/main/java/edu/sdsu/cs/sharepic/classes/Flickr.java
@@ -4,6 +4,6 @@ package edu.sdsu.cs.sharepic.classes;
  * Created by Rakshit Pithadia on 4/15/15.
  * Copyright (c) 2015 Harsh Shah, Rakshit Pithadia. All rights reserved.
  */
-public class Flickr extends AbstractAccount {
+public class Flickr extends Account {
 
 }
