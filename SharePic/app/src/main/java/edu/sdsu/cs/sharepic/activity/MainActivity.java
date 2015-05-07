031b8004dffdaf19674c06b3f0e8e0d87feb0f97
Harsh <shahmharsh@gmail.com>
2015-05-07 15:57:27 -0700

    Removed Card View Demo

--- SharePic/app/src/main/java/edu/sdsu/cs/sharepic/activity/MainActivity.java
+++ SharePic/app/src/main/java/edu/sdsu/cs/sharepic/activity/MainActivity.java
@@ -1,12 +1,10 @@
 package edu.sdsu.cs.sharepic.activity;
 
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,12 +16,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -36,9 +30,7 @@ import edu.sdsu.cs.sharepic.model.FlickrAccount;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-
-    private ListView listView;
+    
     private Button button;
     private Dropbox dropbox;
     private FlickrAccount flickr;
@@ -49,67 +41,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle(Constants.MAIN_TITLE);
 
-//        listView = (ListView) findViewById(R.id.listView);
-//
-//        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-//                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X"};
-//
-//        final ArrayList<String> list = new ArrayList<>();
-//        for (String value : values) {
-//            list.add(value);
-//        }
-//
-//        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-//                android.R.layout.simple_list_item_1, list);
-//        listView.setAdapter(adapter);
-//
         dropbox = Dropbox.getInstance(getApplicationContext());
         if (!dropbox.isLoggedIn()) {
             dropbox.login();
         }
-//        flickr = FlickrAccount.getInstance(this);
-//
+
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-//                if (!flickr.isLoggedIn()) {
-//                    flickr.login();
-//                }
-
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), 123);
             }
         });
-
-//        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
-//        recList.setHasFixedSize(true);
-//        LinearLayoutManager llm = new LinearLayoutManager(this);
-//        llm.setOrientation(LinearLayoutManager.VERTICAL);
-//        recList.setLayoutManager(llm);
-//
-//        ContactAdapter ca = new ContactAdapter(createList(30));
-//        recList.setAdapter(ca);
     }
 
-    private List<ContactInfo> createList(int size) {
-
-        List<ContactInfo> result = new ArrayList<ContactInfo>();
-        for (int i=1; i <= size; i++) {
-            ContactInfo ci = new ContactInfo();
-            ci.name = ContactInfo.NAME_PREFIX + i;
-            ci.surname = ContactInfo.SURNAME_PREFIX + i;
-            ci.email = ContactInfo.EMAIL_PREFIX + i + "@test.com";
-
-            result.add(ci);
-
-        }
-
-        return result;
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
